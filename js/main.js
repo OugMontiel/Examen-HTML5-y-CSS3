321a695c240f2356aa71ef31543479ec8a93f2a0
OugMontiel <88790286+OugMontiel@users.noreply.github.com>
2024-03-21 17:58:59 -0500

    feat: :zap: carrito interactividad
    quedo a mitad la presentacion .. falto organizar la informacion

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function desplegable() {
     var btn = document.getElementById("elemento");
     btn.classList.toggle("visible");
 }
+function desplegableCarrito() {
+    var btn = document.getElementById("elementoCarrito");
+    btn.classList.toggle("visibleCarrito");
+}
